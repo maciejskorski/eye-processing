5c8a8a47f09f91ef5d9c90717e77e45576c6c31b
maciej.skorski <maciej.skorski@gmail.com@>
2022-07-31 09:27:34 +0000

    clean docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,24 @@
 """Sphinx configuration."""
+
+from cgitb import enable
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../source'))
+
+# General information about the project.
 project = "Eye Processing"
 author = "Maciej Skorski"
 copyright = "2022, Maciej Skorski"
+
+# Sources
+source_suffix = ['.md']
+
+# Extensions
 extensions = [
     #"sphinx.ext.autodoc",
     #"sphinx.ext.napoleon",
     #"sphinx_click",
     "myst_parser",
 ]
-autodoc_typehints = "description"
+#autodoc_typehints = "description"
 #html_theme = "furo"
