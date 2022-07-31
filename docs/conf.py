697468c5ec603ef7368849d68ccaf0840a176e16
maciej.skorski <maciej.skorski@gmail.com@>
2022-07-31 09:34:57 +0000

    clean docs

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 from cgitb import enable
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../source'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 
 # -- General configuration ------------------------------------------------
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('../source'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = ['sphinx.ext.autodoc','myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
