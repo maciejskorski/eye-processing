b480e26ab26aa48f627a651e730031ce2fa5e31e
maciej.skorski <maciej.skorski@gmail.com@>
2022-07-31 08:36:47 +0000

    clean docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,8 @@ project = "Eye Processing"
 author = "Maciej Skorski"
 copyright = "2022, Maciej Skorski"
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
+    #"sphinx.ext.autodoc",
+    #"sphinx.ext.napoleon",
     "myst_parser",
 ]
 autodoc_typehints = "description"
