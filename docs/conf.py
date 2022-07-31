c0bbcc91b7e32f902a7517cb08564c933a695508
maciej.skorski <maciej.skorski@gmail.com@>
2022-07-31 08:33:09 +0000

    clean docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,6 @@ copyright = "2022, Maciej Skorski"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    "sphinx_click",
     "myst_parser",
 ]
 autodoc_typehints = "description"
